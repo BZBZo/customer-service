bce6eb69bac54f39a55f5a1dbf89d5e4d5b790af
Ninky0 <2020113392@dgu.ac.kr>
2024-12-09 16:04:34 +0900

    init : setting

---
+++ src/main/java/com/example/spring/bzcustomerservice/BzCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.bzcustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BzCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BzCustomerServiceApplication.class, args);
+    }
+
+}
