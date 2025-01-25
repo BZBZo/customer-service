5177fe28e9abba4a9229ac57082f77f50b6c899c
Ninky0 <2020113392@dgu.ac.kr>
2025-01-25 15:17:25 +0900

    feat : purchase list, try-catch

--- src/main/java/com/example/spring/bzcustomerservice/repository/PurchaseRepository.java
+++ src/main/java/com/example/spring/bzcustomerservice/repository/PurchaseRepository.java
@@ -1,9 +1,14 @@
 package com.example.spring.bzcustomerservice.repository;
 
+import com.example.spring.bzcustomerservice.dto.PurchaseHistoryDTO;
 import com.example.spring.bzcustomerservice.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+    List<Purchase> findAllByMemberNo(Long memberNo);
 }
