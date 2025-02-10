0b0b2377a1326d354e3b9a34314a6811390e5ea4
dlghdu <fhrmsl12@gmail.com>
2025-02-10 14:26:51 +0900

    feat: PurchaseList Page

--- src/main/java/com/example/spring/bzcustomerservice/repository/PurchaseRepository.java
+++ src/main/java/com/example/spring/bzcustomerservice/repository/PurchaseRepository.java
@@ -1,6 +1,8 @@
 package com.example.spring.bzcustomerservice.repository;
 
 import com.example.spring.bzcustomerservice.entity.Purchase;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
-    List<Purchase> findAllByMemberNo(Long memberNo);
+    Page<Purchase> findAllByMemberNo(Long memberNo, Pageable pageable);
 }
